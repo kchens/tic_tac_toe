b5ce03038b21ab8633dd4095716cfe460a9e1683
kchens <kkch3n@gmail.com>
2015-04-16 01:05:30 -0700

    adds working human player game

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,142 +1,194 @@
-class GameModel
-  attr_reader :board, :current_player
+module TicTacToe
+  class GameModel
+    attr_reader :board, :current_player
 
-  def initialize(first_player_factory, second_player_factory)
-    # create a board with nums 0-8
-    @board = Array.new(9) { |num| num }
-    @player_one = first_player_factory.new(self, "X")
-    @player_two = second_player_factory.new(self, "O")
-    @current_player = @player_one
+    def initialize(first_player_factory, second_player_factory)
+      # create a board with nums 0-8
+      new_board
+      @player_one = first_player_factory.new(self, "X")
+      @player_two = second_player_factory.new(self, "O")
+      @current_player = @player_one
+    end
+
+    def switch_players!
+      @current_player = (@current_player.marker == @player_one.marker) ? (@player_two) : (@player_one)
+    end
+
+    def get_available_positions
+      board.select {|cell| cell != "X" && cell != "O"}
+    end
+
+    def set_position!(chosen_pos)
+      board[chosen_pos] = @current_player.marker
+    end
+
+    def return_winner
+      switch_players!
+      @current_player.marker if winner?
+    end
+
+    def winner?
+      win_filter = Proc.new { |array| return true if array.uniq.count == 1 }
+
+      get_rows.each(&win_filter)
+      get_columns.each(&win_filter)
+      get_diagonals.each(&win_filter)
+
+      # get_rows.each       {|row| return true if row.uniq.count == 1 }
+      # get_columns.each    {|column| return true if column.uniq.count == 1 }
+      # get_diagonals.each  {|diagonal| return true if diagonal.uniq.count == 1 }
+      return false
+    end
+
+    def get_rows
+      board.each_slice(3).to_a
+    end
+
+    def get_columns
+      get_rows.transpose
+    end
+
+    def get_diagonals
+      diagonals = []
+      diagonals << 3.times.map { |num| board[num *= 4] }
+      diagonals << 3.times.map { |num| board[(num += 1) * 2] }
+      diagonals
+    end
+
+    def tie? #full_board?
+      get_available_positions.any? ? false : true
+    end
+
+    def new_board
+      @board = Array.new(9) { |num| num }
+    end
   end
 
-  def switch_players!
-    @current_player = (@current_player.marker == @player_one.marker) ? (@player_two) : (@player_one)
+
+  class Player
+    attr_reader :game, :marker
+    def initialize(game, marker)
+      @game = game
+      @marker = marker
+    end
   end
 
-  def get_available_positions
-    board.select {|cell| cell != "X" && cell != "O"}
+  class HumanPlayer < Player
+
+    def set_position!(chosen_pos)
+      if game.get_available_positions.include?(chosen_pos)
+        game.set_position!(chosen_pos)
+        game.switch_players!
+      else
+        return false
+      end
+    end
   end
 
-  def set_position!(chosen_pos)
-    board[chosen_pos] = @current_player.marker
+
+  class GameController
+
+    def initialize(args = {player_one_type: HumanPlayer, player_two_type: HumanPlayer})
+      @game_model = GameModel.new(args[:player_one_type], args[:player_two_type])
+      @view       = GameView.new
+      print_view
+    end
+
+    def play
+      loop do
+        return if calculate_winner
+        return if calculate_tie
+        add_move
+        print_view
+      end
+    end
+
+    def add_move
+      valid_move = false
+      until valid_move
+        chosen_pos = @view.asks_for_position
+        @game_model.current_player.set_position!(chosen_pos) ? (valid_move = true) : @view.says_position_unavailable
+      end
+      @view.print_players_turn(@game_model.current_player.marker)
+    end
+
+    def calculate_winner
+      if @game_model.winner?
+        @view.print_winner(@game_model.return_winner)
+        end_or_rematch
+      end
+    end
+
+    def calculate_tie
+      if @game_model.tie?
+        @view.print_tied
+        end_or_rematch
+      end
+    end
+
+    def end_or_rematch
+      response = @view.ask_to_play_again
+      if response == "Y"
+        @game_model.new_board
+        print_view
+        play
+      else
+        !false
+      end
+    end
+
+    def print_view
+      @view.print_board(@game_model)
+    end
   end
 
-  def return_winner
-    @current_player.marker if winner?
-  end
 
-  def winner?
-    win_filter = Proc.new { |array| return true if array.uniq.count == 1 }
+  class GameView
 
-    get_rows.each(&win_filter)
-    get_columns.each(&win_filter)
-    get_diagonals.each(&win_filter)
+    def print_board(game_model)
+      cells = game_model.board
+      puts " #{cells[0]}| #{cells[1]}| #{cells[2]}"
+      puts "--+---+--"
+      puts " #{cells[3]}| #{cells[4]}| #{cells[5]}"
+      puts "--+---+--"
+      puts " #{cells[6]}| #{cells[7]}| #{cells[8]}"
+    end
 
-    # get_rows.each       {|row| return true if row.uniq.count == 1 }
-    # get_columns.each    {|column| return true if column.uniq.count == 1 }
-    # get_diagonals.each  {|diagonal| return true if diagonal.uniq.count == 1 }
-    return false
-  end
+    def print_players_turn(player_marker)
+      puts "It's #{player_marker}'s turn."
+    end
 
-  def get_rows
-    board.each_slice(3).to_a
-  end
+    def print_winner(winning_marker)
+      puts "Player #{winning_marker} won!"
+    end
 
-  def get_columns
-    get_rows.transpose
-  end
+    def print_tied
+      puts "You tied."
+    end
 
-  def get_diagonals
-    diagonals = []
-    diagonals << 3.times.map { |num| board[num *= 4] }
-    diagonals << 3.times.map { |num| board[(num += 1) * 2] }
-    diagonals
-  end
+    def asks_for_position
+      puts "Select a position"
+      gets.to_i
+    end
 
-  def clear_board
-    @board = Array.new(9) { |num| num }
-  end
-end
+    def says_position_unavailable
+      puts "Unavailable position, choose another"
+    end
 
-
-class Player
-  attr_reader :game, :marker
-  def initialize(game, marker)
-    @game = game
-    @marker = marker
-  end
-end
-
-class HumanPlayer < Player
-
-  def set_position!(chosen_pos)
-    if game.get_available_positions.include?(chosen_pos)
-      game.set_position!(chosen_pos)
-      game.switch_players!
-    else
-      another_pos = gets.to_i
-      set_position!(another_pos) #recursion is non-performant, but cool
+    def ask_to_play_again
+      response = nil
+      until response == 'Y' || response == 'N'
+        puts 'Play again? (Y/N)'
+        response = gets.chomp.upcase
+      end
+      response
     end
   end
 end
 
-
-class GameController
-
-  def initialize(args = {player_one: Player.new, player_two: Player.new, game_model: GameModel.new, view: GameView.new})
-    game_model = args[:game_model]
-    view       = args[:view]
-    # player_one = args[:player_one]
-    # player_two = args[:player_two]
-  end
-
-  def play
-  end
-
-  def set_player_position!
-
-  end
-
-  def switch_players!
-  end
-
-end
-
-
-class GameView
-  def initialize
-  end
-
-  def print_board(game_model)
-    cells = game_model.board
-    puts " #{cells[0]}| #{cells[1]}| #{cells[2]}"
-    puts "--+---+--"
-    puts " #{cells[3]}| #{cells[4]}| #{cells[5]}"
-    puts "--+---+--"
-    puts " #{cells[6]}| #{cells[7]}| #{cells[8]}"
-  end
-
-  def print_winner(winning_marker)
-    puts "Player #{winning_marker} won!"
-  end
-
-  def print_draw
-    puts "You tied."
-  end
-
-  def asks_for_position
-    puts "Select a position"
-  end
-
-  def says_position_unavailable
-    puts "Unavailable position, choose another"
-  end
-end
-
-
 board_checker = Array.new(9) { |num| num }
-game_model = GameModel.new(HumanPlayer, HumanPlayer)
-view = GameView.new
+game_model = TicTacToe::GameModel.new(TicTacToe::HumanPlayer, TicTacToe::HumanPlayer)
+view = TicTacToe::GameView.new
 
 # controller = GameController.new({game_model})
 
@@ -161,12 +213,12 @@ p "Get Available Positions--"
 p game_model.get_available_positions == [0,2,3,4,5,6,7,8]
 
 p "Clear Board--"
-p game_model.clear_board    == board_checker
+p game_model.new_board    == board_checker
 
 p "Set Position--"
 game_model.current_player.set_position!(0)
 p game_model.board          == ["X",1,2,3,4,5,6,7,8]
-game_model.clear_board
+game_model.new_board
 
 # Win Row
 p "Row--"
@@ -176,7 +228,7 @@ game_model.current_player.set_position!(1)
 game_model.current_player.set_position!(7) #other player
 game_model.current_player.set_position!(2)
 # p view.print_board(game_model)
-p game_model.clear_board    == board_checker
+p game_model.new_board    == board_checker
 
 # Win Column
 p "Column--"
@@ -187,7 +239,7 @@ game_model.current_player.set_position!(2) #other player
 game_model.current_player.set_position!(6)
 # p view.print_board(game_model)
 p game_model.winner?        == true
-game_model.clear_board    == board_checker
+game_model.new_board    == board_checker
 
 # Win Diagonal
 p "Diagonal--"
@@ -200,11 +252,33 @@ game_model.current_player.set_position!(8)
 p game_model.winner?        == true
 
 p "Return Winner --"
-p game_model.return_winner  == "X"
-game_model.clear_board    == board_checker
+p game_model.return_winner  == "O"
+p view.print_board(game_model)
+game_model.new_board    == board_checker
+
+p "Return Tie --"
+game_model.current_player.set_position!(0)
+game_model.current_player.set_position!(1) #other player
+game_model.current_player.set_position!(2)
+game_model.current_player.set_position!(4) #other player
+game_model.current_player.set_position!(7) #other player
+game_model.current_player.set_position!(3)
+game_model.current_player.set_position!(6)
+game_model.current_player.set_position!(8)
+game_model.current_player.set_position!(5)
+p game_model.winner?        == false
+p game_model.tie?        == true
+
+p "Check if board reset"
+p view.print_board(game_model)
+game_model.new_board
+p view.print_board(game_model)
 
 # Unable to move
-p "Player unable to move--until you enter an available number (not 0)"
-game_model.current_player.set_position!(0)
-game_model.current_player.set_position!(0)
+# p "Player unable to move--until you enter an available number (not 0)"
+# game_model.current_player.set_position!(0)
+# game_model.current_player.set_position!(0)
 # p view.print_board(game_model)
+
+my_game = TicTacToe::GameController.new
+my_game.play
